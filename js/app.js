7c8c1c2934b46ec7f9474a96d57ebae40e88399f
Chris Roman <Christopherroman85@gmail.com>
2018-09-12 18:12:33 -0500

    Commit 2 - Get Even answered

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 console.log('It is working!');
 
+// Easy Going
+
 for(let i = 1; i <= 20; i++){
 	console.log(i)
+}
+
+// Get Even
+
+for(let i = 0; i <= 200; i+=2){
+	if(i % 2 === 0){
+		console.log(i);
+	}
 }
