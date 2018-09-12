4ee1abb3628b443b7f790f7ae345ff0780369b64
Chris Roman <Christopherroman85@gmail.com>
2018-09-12 18:39:09 -0500

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -25,3 +25,16 @@ for(let i = 1; i <= 20; i++){
 		console.log("Love me, pet me! HSSSSSS!");
 	}
 }
+
+// Fizz Buzz
+
+for(let i = 1; i <= 100; i++){
+	console.log(i)
+	if(i % 3 === 0 && i % 5 === 0){
+		console.log("FizzBuzz");
+	} else if(i % 3 === 0){
+		console.log("Fizz")
+	} else if(i % 5 === 0){
+		console.log("Buzz")
+	} 
+}
