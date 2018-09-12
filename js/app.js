dcffe88cab53b828c90e7cda0ab1fb68fce48ebc
Chris Roman <Christopherroman85@gmail.com>
2018-09-12 18:24:35 -0500

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -13,3 +13,15 @@ for(let i = 0; i <= 200; i+=2){
 		console.log(i);
 	}
 }
+
+// Excited Kitten
+
+for(let i = 1; i <= 20; i++){
+	if(i % 2 === 0){
+		const response = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+		const catResponse = response[Math.floor(response.length * Math.random())]
+		console.log(catResponse)
+	} else {
+		console.log("Love me, pet me! HSSSSSS!");
+	}
+}
